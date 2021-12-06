f303ef805a6ab548e98294ce5276737fadf2d00a
inthej <inthej12@gmail.com>
2021-12-06 21:34:41 +0900

    Update Header Component

--- frontend/README.md
+++ frontend/README.md
@@ -12,3 +12,6 @@ $ yarn add --dev --exact prettier
 ## Reference
 - Fonts: [https://flamingotiger.github.io/frontend/react/react-add-font/#%EA%B5%AC%EA%B8%80-%EC%9B%B9%ED%8F%B0%ED%8A%B8%EB%A5%BC-%ED%86%B5%ED%95%B4%EC%84%9C-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0](https://flamingotiger.github.io/frontend/react/react-add-font/#%EA%B5%AC%EA%B8%80-%EC%9B%B9%ED%8F%B0%ED%8A%B8%EB%A5%BC-%ED%86%B5%ED%95%B4%EC%84%9C-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0)
 - NavLink: [https://velog.io/@ksh4820/React-NavLink](https://velog.io/@ksh4820/React-NavLink)
+- CSS Flex: [https://heropy.blog/2018/11/24/css-flexible-box/](https://heropy.blog/2018/11/24/css-flexible-box/)
+- CSS Position Sticky: [https://juahnpop.tistory.com/182](https://juahnpop.tistory.com/182)
+- 신문사 폰트: [https://dadoc.or.kr/1064](https://dadoc.or.kr/1064)
--- frontend/src/pages/NewsPage.js
+++ frontend/src/pages/NewsPage.js
@@ -1,10 +1,26 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+import styled from "styled-components";
 import {CategoryType} from "../common/AppTypes";
 import Categories from "./components/Categories";
 import NewsList from "./components/NewsList";
 
+const Header = styled(NavLink)`
+  font-family: 'Nanum Gothic ExtraBold', serif;
+  font-weight: 900;
+  font-size: 80px;
+  text-transform: uppercase;
+  display: inline-block;
+  line-height: 72px;
+  margin-bottom: 20px;
+
+  text-decoration: none;
+  color: #2f2f2f;
+`;
+
 const NewsPage = ({match}) => {
   const category = match.params.category || CategoryType.all;
+
   return (
     <>
       <div className="head">
@@ -17,7 +33,7 @@ const NewsPage = ({match}) => {
           {/*  <br/>*/}
           {/*  <span>Wind: 7km/h SSE; Ther: 21°C; Hum: 82%</span>*/}
           {/*</div>*/}
-          <header>현뉴스</header>
+          <Header to="/">현뉴스</Header>
         </div>
       </div>
 
